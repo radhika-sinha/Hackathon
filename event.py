d95748f5388b43664658da9a24bcb7035f6d709d
Abdullah Riaz <ariaz988@gmail.com>
2022-01-13 21:26:31 -0800

    getters and setters for host

--- event.py
+++ event.py
@@ -1,15 +1,23 @@
 class Event():
     def __init__(self, hostInstance, name, date, address, county, state, numVaxRequired, recentTestRequired):
-        self.hostName = hostInstance.name
-        self.hostPhoneNumber = hostInstance.phoneNumber
-        self.hostEmail = hostInstance.email
-        self.name = name
-        self.date = date
-        self.address = address
-        self.county = county
-        self.state = state
-        self.numVaxRequired = numVaxRequired
-        self.recentTestRequired = recentTestRequired
+        self._hostName = hostInstance.name
+        self._hostPhoneNumber = hostInstance.phoneNumber
+        self._hostEmail = hostInstance.email
+        self._name = name
+        self._date = date
+        self._address = address
+        self._county = county
+        self._state = state
+        self._numVaxRequired = numVaxRequired
+        self._recentTestRequired = recentTestRequired
+    
+    #getters and setters
+    
+    @property
+    def eventHostName(self):
+        return self._hostName
+    
+    
         
         
         
--- host.py
+++ host.py
@@ -2,19 +2,42 @@ from event import Event
 from hostConsole import hostConsole
 class Host():
     def __init__(self,name,phoneNumber,email):
-        self.name = name
-        self.phoneNumber = phoneNumber
-        self.email = email
-        self.events = set()
-        self.console = hostConsole(self)
+        self._name = name
+        self._phoneNumber = phoneNumber
+        self._email = email
+        self._events = set()
+        self._console = hostConsole(self)
+    
+    #setters and getters
+    @property
+    def hostName(self):
+        return self._name
+    @hostName.setter
+    def hostName(self, newName):
+        self._name = newName
+    
+    @property
+    def hostPhoneNumber(self):
+        return self._phoneNumber
+    @hostPhoneNumber.setter
+    def hostPhoneNumber(self, newNumber):
+        self._phoneNumber = newNumber
+    
+    @property
+    def hostEmail(self):
+        return self._email
+    @hostEmail.setter
+    def hostEmail(self, newEmail):
+        self._email = newEmail
+    
+    #methods
     
     def hostCreateEvent(self, hostInstance, name, date, address, county, state, numVaxRequired, recentTestRequired):
         self.events.add(Event(self, name, date, address, county, state, numVaxRequired, recentTestRequired))
-    
+        
     def hostDeleteEvent(self, eventInstance):
         self.events.remove(eventInstance)
     
-    
-        
+            
         
         
